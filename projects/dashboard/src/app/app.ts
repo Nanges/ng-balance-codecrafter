e1980dc63bca3175bb360d9c529d069bdde075ca
Nanges <laha.olivier@gmail.com>
2026-08-05 21:23:34 +0200

    feat: add button

--- projects/dashboard/src/app/app.ts
+++ projects/dashboard/src/app/app.ts
@@ -1,6 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ZardButtonComponent } from '@/ui/primitives/button';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +7,4 @@ import { ZardButtonComponent } from '@/ui/primitives/button';
   template:`<router-outlet/>`,
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('dashboard');
-}
+export class App {}
--- projects/dashboard/src/app/login-page/login-page.component.ts
+++ projects/dashboard/src/app/login-page/login-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ZardButtonComponent } from '@/ui/primitives/button';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ZardButtonComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
-export class LoginPageComponent {
-
-}
+export class LoginPageComponent {}
