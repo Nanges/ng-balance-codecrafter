d30a82d3921b094c8d748b52e610ca7d6fbdaf28
Nanges <laha.olivier@gmail.com>
2026-08-05 21:53:08 +0200

    login page

--- projects/dashboard/src/app/login-page/login-page.component.ts
+++ projects/dashboard/src/app/login-page/login-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { ZardButtonComponent } from '@/ui/primitives/button';
 import { ZardCardComponent } from '@/ui/primitives/card'
+import { ZardFormFieldComponent, ZardFormLabelComponent, ZardFormControlComponent } from '@/ui/primitives/form';
+import {ZardInputComponent} from '@/ui/primitives/input';
 
 @Component({
   selector: 'app-login-page',
-  imports: [ZardButtonComponent, ZardCardComponent],
+  imports: [
+    ZardButtonComponent,
+    ZardCardComponent,
+    ZardInputComponent,
+    ZardFormFieldComponent,
+    ZardFormLabelComponent,
+    ZardFormControlComponent
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
