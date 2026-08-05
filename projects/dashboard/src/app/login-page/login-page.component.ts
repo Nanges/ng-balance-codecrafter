e219688a4f8df88623ea92330c6183bf7340b684
Nanges <laha.olivier@gmail.com>
2026-08-05 23:16:27 +0200

    feat: update login page

--- projects/dashboard/src/app/login-page/login-page.component.ts
+++ projects/dashboard/src/app/login-page/login-page.component.ts
@@ -2,9 +2,14 @@ import { Component, inject, signal } from '@angular/core';
 import { ZardButtonComponent } from '@/ui/primitives/button';
 import { ZardCardComponent } from '@/ui/primitives/card'
 import { ZardFormFieldComponent, ZardFormLabelComponent, ZardFormControlComponent } from '@/ui/primitives/form';
-import {ZardInputComponent} from '@/ui/primitives/input';
+import { ZardInputComponent } from '@/ui/primitives/input';
+import { ZardAlertComponent } from '@/ui/primitives/alert';
 import { AuthApiService, GetTokenRequest } from 'api'
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { provideIcons } from '@ng-icons/core';
+import { lucideAlertTriangle } from '@ng-icons/lucide';
+import { catchError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
@@ -15,10 +20,12 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
     ZardFormFieldComponent,
     ZardFormLabelComponent,
     ZardFormControlComponent,
+    ZardAlertComponent,
     ReactiveFormsModule,
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
+  viewProviders: [provideIcons({ lucideAlertTriangle })]
 })
 export class LoginPageComponent {
   readonly #api = inject(AuthApiService);
@@ -28,13 +35,28 @@ export class LoginPageComponent {
   });
 
   protected readonly loading = signal<boolean>(false);
+  protected readonly showInvalidCredentials = signal<boolean>(false);
 
   login(){
     this.loading.set(true);
+    this.showInvalidCredentials.set(false);
+
     this.#api.login({
       user:this.form.value
-    } as GetTokenRequest).subscribe(() => {
-      this.loading.set(false);
+    } as GetTokenRequest)
+    .subscribe({
+      error:(err: HttpErrorResponse) => {
+        if(err.status === 401){
+          this.showInvalidCredentials.set(true);
+          return;
+        }
+      },
+      next:() => {
+        // TODO
+      },
+      complete:() => {
+        this.loading.set(false);
+      }
     });
   }
 }
