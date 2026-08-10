e4a7ef331407d5af07ea99c3687741d6b2406189
Nanges <laha.olivier@gmail.com>
2026-08-10 02:05:42 +0200

    setup dashboard component

--- projects/dashboard/src/app/dashboard/dashboard.component.ts
+++ projects/dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutImports } from '@/ui/primitives/layout';
+import { ZardMenuImports } from '@/ui/primitives/menu';
+import { ZardButtonComponent } from '@/ui/primitives/button';
+import { NgIcon, provideIcons } from "@ng-icons/core";
+import { lucideSettings, lucideUserCircle } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ZardButtonComponent,
+    LayoutImports,
+    ZardMenuImports,
+    NgIcon
+],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
+  viewProviders:[provideIcons({ lucideSettings, lucideUserCircle })]
 })
 export class DashboardComponent {
-
+  protected readonly collapased = signal<boolean>(false);
 }
